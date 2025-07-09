6975ea3ed4c68453e0b8592840c3b53e77a23973
Juniorelmusico <129994805+Juniorelmusico@users.noreply.github.com>
2025-07-08 21:48:49 -0500

    first commit

---
+++ Udla-Workshop-Design-Patterns-main/DesignPatterns/Infraestructure/DependencyInjection/ServicesConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatterns.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DesignPatterns.Infraestructure.DependencyInjection
+{
+    public class ServicesConfiguration
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IVehicleRepository, MyVehiclesRepository>();
+        }
+    }
+}
---
+++ Udla-Workshop-Design-Patterns-main/DesignPatterns/Models/Motocycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Models
+{
+    public class Motocycle : Vehicle
+    {
+        public override int Tires { get => 2; }
+
+        public Motocycle(string color, string brand, string model) : base(color, brand, model, 5)
+        {
+        }
+    }
+}
